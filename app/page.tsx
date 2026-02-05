91f8abcba5976896ebe6a5917358bb0e28b19190
mss879 <130873154+mss879@users.noreply.github.com>
2026-02-05 21:17:29 +0530

    Remove TechMarquee section from homepage

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import { HeroSection } from '@/components/hero-section';
 import { CategoryMarquee } from '@/components/category-marquee';
 import { AboutSection } from '@/components/about-section';
 import { ServiceSection } from '@/components/service-section';
-import { TechMarquee } from '@/components/tech-marquee';
 import { ApproachSection } from '@/components/approach-section';
 import { BlogsSection } from '@/components/blogs-section';
 import { Footer } from '@/components/footer';
@@ -16,7 +15,6 @@ export default function Home() {
       <CategoryMarquee />
       <AboutSection />
       <ServiceSection />
-      <TechMarquee />
       <ApproachSection />
       <BlogsSection />
       <Footer />
